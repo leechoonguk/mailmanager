2c38c0be69ef265aa4756477efc0341669062284
leechoonguk <dcf5112@gmail.com>
2021-03-25 22:35:23 +0900

    init

---
+++ src/main/java/com/develop/mailmanager/MailmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.develop.mailmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develop/mailmanager/MailmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.mailmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
